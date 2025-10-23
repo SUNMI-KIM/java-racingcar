a86ec22779c6a4c7bae3e4f0800ded95c04f6b55
SUNMI <hariaus@naver.com>
2025-10-23 15:30:02 +0900

    feat(MoveDecision): 차 동작 여부 판단 기능 구현
    - 0~9 사이의 핸덤 값 생성
    - 값이 4 이상일 경우 이동하도록(true) 반환

---
+++ src/main/java/racingcar/model/MoveDecision.java
@@ -0,0 +1,18 @@
+package racingcar.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class MoveDecision {
+
+    private static final int MOVE_CONDITION = 4;
+    private static final int MIN_RANDOM_NUMBER = 0;
+    private static final int MAX_RANDOM_NUMBER = 9;
+
+    private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER);
+    }
+
+    public boolean canMove() {
+        return generateRandomNumber() >= MOVE_CONDITION;
+    }
+}
